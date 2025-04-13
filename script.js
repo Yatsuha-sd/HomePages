ce09248bbcc6f589e2f65a3126e70b2a71d89bbf
Yatsuha <gobou1192noatti@gmail.com>
2025-04-14 02:21:04 +0900

    test

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function f(k) {
 f(-1);
 
 addEventListener('scroll', e => f(+getComputedStyle(document.body).getPropertyValue('--k')))
+
+document.addEventListener("DOMContentLoaded", function () {
+	document.querySelectorAll("article").forEach(article => {
+	  article.addEventListener("click", function () {
+		// カスタムプロパティから URL を取得
+		let url = getComputedStyle(this).getPropertyValue("--link").trim();
+		url = url.replace(/^url\(["']?/, "").replace(/["']?\)$/, ""); // "url()" を除去
+		if (url) {
+		  window.open(url, "_blank"); // 新しいタブで開く
+		}
+	  });
+	});
+  });
