c4777c983338217b668c069a4dc75608cf106fd1
Yatsuha <gobou1192noatti@gmail.com>
2025-04-14 02:02:55 +0900

    test

--- script.js
+++ script.js
@@ -1,39 +1,8 @@
-let root = document.documentElement;
+function f(k) {
+	if(Math.abs(k) > .5)
+		scrollTo(0, .5*(k - Math.sign(k) + 1)*(document.documentElement.offsetHeight - window.innerHeight))
+}
 
-root.addEventListener("pointermove", e => {
-  root.style.setProperty("--x", e.clientX);
-  root.style.setProperty("--y", e.clientY);
-});
+f(-1);
 
-let n = getComputedStyle(root).getPropertyValue("--n");
-let threshold = n / 2;
-
-addEventListener("scroll", e => {
-  let current = scrollY / innerHeight;
-  if (current >= threshold) scrollTo(0, 1);
-  if (current < 1) scrollTo(0, innerHeight * (threshold - 1));
-});
-
-fetch('./data.json')
-  .then(response => response.json())
-  .then(data => {
-    const section = document.querySelector('.assembly');
-    data.forEach((item, index) => {
-      const article = document.createElement('article');
-      article.style = `--i: ${index}; --url: url(${item.img}); ${item.pos ? `--pos: ${item.pos};` : ''}`;
-
-      article.innerHTML = `
-        <header>
-          <h2>${item.title}</h2><em>${item.latin}</em>
-        </header>
-        <figure>
-          <img src="${item.img}" alt="${item.alt}" />
-          <figcaption>by <a href="${item.link}" target="_blank">${item.caption}</a></figcaption>
-        </figure>
-      `;
-      section.appendChild(article);
-    });
-
-    root.style.setProperty("--n", data.length);
-  })
-  .catch(err => console.error('JSON読み込みエラー:', err));
+addEventListener('scroll', e => f(+getComputedStyle(document.body).getPropertyValue('--k')))
